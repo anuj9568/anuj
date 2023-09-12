deb3c0de1c2d3ad05f0cccd6aac3722d9460df02
Anuj Kumar Gupta <a.kumar-gupta@enbw.com>
2023-09-12 15:03:20 +0530

    Updated files

---
+++ src/components/Blogs.jsx
@@ -0,0 +1,21 @@
+/* eslint-disable quotes */
+import React from "react";
+
+const styles = {
+  spinnerContainerStyle: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100%",
+  },
+};
+
+function Blogs() {
+  return (
+    <div style={styles.spinnerContainerStyle}>
+      <h3>This page is under constructon, stay tuned for more update😀.</h3>
+    </div>
+  );
+}
+
+export default Blogs;
